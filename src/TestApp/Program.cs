ee6ac551d3aa3911dfaaeeb52b18645c83ae32a9
ihsanndamar <111857789+ihsanndamar@users.noreply.github.com>
2024-05-24 23:37:37 +0300

    feat: tested end to end and integration with using console app

---
+++ src/TestApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using ConfigurationManagement;
+
+
+
+
+ConfigurationReader configurationReader = new ConfigurationReader("SERVICE-A", "mongodb://localhost:27017", 10000);
+
+
+
+int a = 0;
+while (true)
+{
+    Console.WriteLine(a.ToString() + " - " + configurationReader.GetValue<int>("MaxItemCount"));
+    Thread.Sleep(1000); // 1000 milisaniye = 1 saniye
+    a++;
+}
+
+
+
+
