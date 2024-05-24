eec6d028b1505df72ad94aaf5b7683a3170bd506
ihsanndamar <111857789+ihsanndamar@users.noreply.github.com>
2024-05-24 23:36:46 +0300

    feat: unit test was added

---
+++ src/ConfigurationUnitTest/UnitTest.cs
@@ -0,0 +1,25 @@
+﻿using ConfigurationManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigurationUnitTest
+{
+    public class UnitTest
+    {
+        [Fact]
+        public void ConfigurationReaderUnitTest()
+        {
+            // Arrange
+            var reader = new ConfigurationReader("SERVICE-A", "mongodb://localhost:27017", 2000);
+
+            // Act
+            var siteName = reader.GetValue<string>("SiteName");
+
+            // Assert
+            Assert.Equal("soty.io", siteName);
+        }
+    }
+}
