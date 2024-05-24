0ccb5a2fa814eb54260be2c4ba74c8e6fe007a22
ihsanndamar <111857789+ihsanndamar@users.noreply.github.com>
2024-05-24 15:53:57 +0300

    feat: db connection and basix of class were added

---
+++ src/ConfigurationManagement/ConfigurationReader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigurationManagement
+{
+    public class ConfigurationReader
+    {
+        public string applicationName;
+        public string connectionString;
+        public int refreshTimerIntervalInMs;
+
+        public ConfigurationReader(string applicationName, string connectionString, int refreshTimerIntervalInMs)
+        {
+            this.applicationName = applicationName;
+            this.connectionString = connectionString;
+            this.refreshTimerIntervalInMs = refreshTimerIntervalInMs;
+        }
+
+
+        public bool GetValue<T>(string key)
+        {
+            return true;
+        }
+    }
+}
---
+++ src/ConfigurationManagement/ConfigurationRecordsUtilities.cs
@@ -0,0 +1,79 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Core.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ConfigurationManagement
+{
+    public class ConfigurationRecordsUtilities
+    {
+        private string connectionString;
+
+        public ConfigurationRecordsUtilities(string connectionString)
+        {
+            connectionString = connectionString;
+        }
+
+        public List<ConfigurationModel> GetConfigurationRecords(string applicationName)
+        {
+            //mongodb connection
+            var client = new MongoClient(connectionString);
+
+            //created filter tp get active records of appName 
+            var filter = Builders<BsonDocument>.Filter.Eq("ApplicationName", applicationName) & Builders<BsonDocument>.Filter.Eq("IsActive", true);
+
+            //get records from db
+            var documents = client.GetDatabase("ConfigruationManagementDB").GetCollection<BsonDocument>("ConfigurationRecords").Find(filter).ToList();
+
+            return ConfigurationModelMapping(documents);
+        }
+
+        private List<ConfigurationModel> ConfigurationModelMapping(List<BsonDocument> documents)
+        {
+            var configurations = new List<ConfigurationModel>();
+            foreach (var document in documents)
+            {
+                var name = document["Name"].AsString;
+                var type = document["Type"].AsString;
+                var value = document["Value"].ToString();
+
+                configurations.Add(new ConfigurationModel(name, ConvertToType(value, type)));
+            }
+            return configurations;
+        }
+
+        private object ConvertToType(string value, string type)
+        {
+            switch (type.ToLower())
+            {
+                case "string":
+                    return value;
+                case "int":
+                    return int.Parse(value);
+                case "bool":
+                    return bool.Parse(value);
+                case "double":
+                    return double.Parse(value);
+                default:
+                    throw new Exception("Unknown type");
+            }
+        }
+    }
+
+    public class ConfigurationModel
+    {
+        public ConfigurationModel(string name, object value)
+        {
+            Name = name;
+            Value = value;
+        }
+
+        public string Name { get; set; }
+        public object Value { get; set; }
+    }
+}
